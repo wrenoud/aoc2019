d0c0e7bac2479677a0c261d32e061fa2de36b8ef
Weston Renoud <wrenoud@gmail.com>
2019-12-26 13:30:25 +0100

    day 11 solution
    also moved Coord class from day 3 to a lib to reuse and added hash and equality functions

--- day_03.py
+++ day_03.py
@@ -1,24 +1,6 @@
 import util
 from typing import Optional, List, Tuple
-
-
-class Coord(object):
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return Coord(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Coord(self.x - other.x, self.y - other.y)
-
-    def __len__(self) -> int:
-        """returns manhattan distance from origin"""
-        return abs(self.x) + abs(self.y)
-
-    def __repr__(self) -> str:
-        return "{}({},{})".format(self.__class__.__name__, self.x, self.y)
+from lib import Coord
 
 
 class Segment(object):
---
+++ day_11.py
@@ -0,0 +1,139 @@
+import util
+from intcode import computer
+from lib import Coord
+from copy import copy
+
+BLACK = 0
+WHITE = 1
+
+class Canvas(object):
+    def __init__(self):
+        self.canvas = {}
+
+    def paint(self, position: Coord, color:int):
+        self.canvas[position] = color
+
+    def color(self, position: Coord):
+        if position in self.canvas:
+            return self.canvas[position]
+        else:
+            return None
+
+    def __repr__(self):
+        mins = Coord(0,0)
+        maxs = Coord(0,0)
+        for key in self.canvas.keys():
+            mins.x = min(key.x, mins.x)
+            mins.y = min(key.y, mins.y)
+            maxs.x = max(key.x, maxs.x)
+            maxs.y = max(key.y, maxs.y)
+        
+        canvas = []
+        for i in range(maxs.y-mins.y + 1):
+            canvas.append(['.',]*(maxs.x-mins.x + 1))
+
+        for key, color in self.canvas.items():
+            pos = key - mins
+            if color == BLACK:
+                canvas[pos.y][pos.x] = ' '
+            else:
+                canvas[pos.y][pos.x] = "#"
+
+        canvas.reverse()
+
+        return "\n".join("".join(str(v) for v in line) for line in canvas)
+
+
+
+class Robot(object):
+    def __init__(self, program, verbose=False):
+        self.position = Coord(0,0)
+        self.direction = 0
+        self.hull = Canvas()
+        # pass the camera generator as stdin to the program
+        self.stdout = computer.Run(program[:], self.Camera(), verbose)
+
+    def Camera(self):
+        while True:
+            color = self.hull.color(self.position)
+            if color is None:
+                yield BLACK # assume black if unpainted
+            else:
+                yield color
+
+    def Paint(self):
+        while True:
+            try:
+                color = next(self.stdout)
+                rotation = next(self.stdout)
+            except StopIteration:
+                return
+
+            position = copy(self.position)
+            self.hull.paint(position, color)
+
+            # execute turn
+            if rotation:
+                self.direction += 1
+            else:
+                self.direction -= 1
+            self.direction %= 4
+
+            # advance robot
+            if self.direction == 0:
+                self.position.y += 1
+            elif self.direction == 1:
+                self.position.x += 1
+            elif self.direction == 2:
+                self.position.y -= 1
+            elif self.direction == 3:
+                self.position.x -= 1
+            
+            yield (color, position)
+
+
+def part1(data):
+    # test job
+    robot = Robot([], False)
+    # replace the stdout from the program with a preplanned route
+    robot.stdout = (v for v in [
+        WHITE,0, #
+        BLACK,0, #
+        WHITE,0, #
+        WHITE,0, #
+        BLACK,1, #
+        WHITE,0, #
+        WHITE,0])
+    # we have to manually advance the camera output as no program will be asking for input
+    camera = robot.Camera()
+    for color, position in robot.Paint():
+        next(camera)
+    print(robot.hull)
+
+    # actual paint job
+    robot = Robot(data, False)
+    for color, position in robot.Paint():
+        pass
+
+    util.Answer(1, len(robot.hull.canvas.keys()))
+
+        
+def part2(data):
+    robot = Robot(data, False)
+
+    # robot expects to start on a white panel
+    robot.hull.paint(Coord(0,0), WHITE)
+
+    for color, position in robot.Paint():
+        pass
+
+    util.Answer(2, None)
+    print(robot.hull)
+
+
+if __name__ == "__main__":
+    data = util.ReadPuzzle()
+    data = list(int(v) for v in data[0].split(','))
+
+    part1(data)
+    part2(data)
